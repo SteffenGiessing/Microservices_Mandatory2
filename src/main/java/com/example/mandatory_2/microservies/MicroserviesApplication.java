f5cb0149ff893bb9b821a5723bc52e19a3416906
stef2761 <Snookey1995@hotmail.com>
2020-05-06 15:02:28 +0200

    First Commit

---
+++ src/main/java/com/example/mandatory_2/microservies/MicroserviesApplication.java
@@ -0,0 +1,20 @@
+package com.example.mandatory_2.microservies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MicroserviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviesApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "hallo";
+    }
+}
